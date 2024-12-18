48f41a36446dc14a4277d0ed402d7f7333983873
Guilherme Bomfim <guigui_bsg@hotmail.com>
2024-12-18 13:33:49 -0300

    Criando a classe Posicao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tabuleiro;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Posicao p;
+
+        p = new Posicao(3, 4);
+
+        Console.WriteLine("Posição: " + p);
+    }
+}
