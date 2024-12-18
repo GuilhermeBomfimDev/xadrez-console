d89ee38486082c7a60b77f3c142def8d6f698d7b
Guilherme Bomfim <guigui_bsg@hotmail.com>
2024-12-18 13:51:04 -0300

    Começando a implementar o tabuleiro

--- Program.cs
+++ Program.cs
@@ -4,10 +4,6 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Posicao p;
-
-        p = new Posicao(3, 4);
-
-        Console.WriteLine("Posição: " + p);
+        Tabuleiro tab = new Tabuleiro(8, 8);
     }
 }
