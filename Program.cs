1297db8754a03f87a961567b2bf9f86d7fbcb7ae
Guilherme Bomfim <guigui_bsg@hotmail.com>
2024-12-19 21:20:46 -0300

    Controlando peças capturadas

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using tabuleiro;
 using xadrez;
+using System;
 
 namespace xadrez_console
 {
@@ -15,13 +16,8 @@ namespace xadrez_console
                 {
                     try
                     {
-
-
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
@@ -48,7 +44,7 @@ namespace xadrez_console
             catch (TabuleiroException ex) {
                 Console.WriteLine(ex.Message);
             }
-            
+            Console.ReadLine();
         }
     }
 }
